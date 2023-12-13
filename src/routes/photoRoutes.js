194b29a9e32950fc5b0faec44d8ef94eda02eddb
Lucas Montenegro <144347120+lukinhas563@users.noreply.github.com>
2023-12-13 10:29:01 -0300

    Photos db models update

--- src/routes/photoRoutes.js
+++ src/routes/photoRoutes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import photoControler from '../controllers/PhotoController';
+import loginRequired from '../middlewares/loginRequired';
 
 const router = new Router();
 
-router.post('/', photoControler.store);
+router.post('/', loginRequired, photoControler.store);
 
 export default router;
