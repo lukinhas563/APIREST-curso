d271e73277de891cb1b468b22e4a5a0a768a6f15
Lucas Montenegro <144347120+lukinhas563@users.noreply.github.com>
2023-12-13 09:23:39 -0300

    Files update

--- src/routes/photoRoutes.js
+++ src/routes/photoRoutes.js
@@ -1,13 +1,8 @@
 import { Router } from "express";
-import multer from "multer";
-
 import photoControler from '../controllers/PhotoController';
-import multerConfig from "../config/multerConfig";
 
-const upload = multer(multerConfig);
 const router = new Router();
 
-router.post('/', upload.single('foto'), photoControler.store);
-
+router.post('/', photoControler.store);
 
 export default router;
