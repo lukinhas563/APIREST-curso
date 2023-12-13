8ddcfa3e8660eecc3e0e3ee34d948514322ec8cf
Lucas Montenegro <144347120+lukinhas563@users.noreply.github.com>
2023-12-13 11:42:52 -0300

    Build update

---
+++ dist/database/seeds/20231212191929-criar-usuarios.js
@@ -0,0 +1,37 @@
+"use strict";'use strict';
+const bcryptjs = require('bcryptjs');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface) {
+
+    await queryInterface.bulkInsert(
+      'users',
+      [
+        {
+          nome: 'Luiz 1',
+          email: 'luiz1@email.com',
+          password_hash: await bcryptjs.hash('123456', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          nome: 'Luiz 2',
+          email: 'luiz2@email.com',
+          password_hash: await bcryptjs.hash('654321', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          nome: 'Luiz 3',
+          email: 'luiz3@email.com',
+          password_hash: await bcryptjs.hash('147258', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ], {});
+
+  },
+
+  async down(queryInterface, Sequelize) { }
+};
--- src/server.js
+++ src/server.js
@@ -1,6 +1,4 @@
 import app from "./app";
 
-const port = 3002;
-app.listen(port, () => {
-    console.log(`CTRL + Clique em http://localhost:${port}`);
-});
+const port = process.env.APP_PORT;
+app.listen(port);
