a641d310fa825e5e0e6e6ee4d774da1f618ec81a
Lucas Montenegro <144347120+lukinhas563@users.noreply.github.com>
2023-12-14 16:57:25 -0300

    app update

--- src/app.js
+++ src/app.js
@@ -41,7 +41,7 @@ class App {
     }
 
     middlewares() {
-        this.app.use(cors(corOptions));
+        this.app.use(cors());
         this.app.use(helmet());
         this.app.use(express.urlencoded({ extended: true }));
         this.app.use(express.json());
