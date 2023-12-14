1ba849f31c113a9fa9a5afe114df77e841d262d1
Lucas Montenegro <144347120+lukinhas563@users.noreply.github.com>
2023-12-14 09:00:04 -0300

    Update build

--- dist/app.js
+++ dist/app.js
@@ -18,8 +18,7 @@ var _console = require('console');
 
 const whiteListe = [
     'https://apirest-cursojs.onrender.com',
-    'https://localhost:3002',
-    'https://www.google.com.br'
+    'https://localhost:3002'
 ];
 
 const corOptions = {
